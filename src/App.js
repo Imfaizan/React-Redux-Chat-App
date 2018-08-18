01bbafa0c4459f8844384f7f465f4f01c76f6b99
Faizan Gaima <faizangaima@Faizans-MacBook-Pro.local>
2018-08-19 00:39:53 +0530

    Functional components and fake data added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 import Sidebar from './Sidebar';
 import Main from './Main';
 import './App.css';
+import store from './store';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Sidebar />
-        <Main />
-      </div>
-    );
-  }
+const App = () => {
+  const { contacts } = store.getState();
+  return (
+    <div className="App">
+      <Sidebar contacts={_.values(contacts)} />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
